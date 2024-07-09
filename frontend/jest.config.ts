5fc32ec7a1871e80d20e95a4396cc5c8c047a306
cursogco <jps012009@yahoo.com.br>
2024-07-09 18:27:11 +0000

    Configuração do jest

---
+++ frontend/jest.config.ts
@@ -0,0 +1,21 @@
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js';
+
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+  '^@/components/(.*)$': '<rootDir>/components/$1'
+  }
+};
+
+export default createJestConfig(config);
+
